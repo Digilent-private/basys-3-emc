fc3050a950914c7b85a0cdade3122906365e2776
ArtVVB <arthur.brown@digilent.com>
2026-02-12 09:04:44 -0800

    Ignore ILA probes file if it doesn't exist
    Lack of ILA was causing an error to be logged in programming
    
    (cherry picked from commit d0459cf0b37993678479ca551c887a9e81e2fbfb)

--- host/program_device.tcl
+++ host/program_device.tcl
@@ -5,7 +5,11 @@ set device [lindex [get_hw_devices xc7a35t_0] 0]
 set scriptdir [file dirname [info script]]
 current_hw_device $device
 refresh_hw_device -update_hw_probes false
-set_property PROBES.FILE $scriptdir/design_1_wrapper.ltx $device
+if {[file exists $scriptdir/design_1_wrapper.ltx]} {
+    set_property PROBES.FILE $scriptdir/design_1_wrapper.ltx $device
+} else {
+    puts "Couldn't find design_1_wrapper.ltx, not using it for programming"
+}
 set_property FULL_PROBES.FILE {} $device
 set_property PROGRAM.FILE $scriptdir/top_out.bit $device
 program_hw_devices $device
